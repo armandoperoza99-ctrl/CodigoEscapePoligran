fe3197833f9ed17bfe5b5f2d8830cac0669a7bf0
lvromjim <lauraromero0410@gmail.com>
2026-06-08 15:37:08 -0500

    Add authorship comments to TileManager.java
    Added authorship comments to TileManager.java

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,3 +1,12 @@
+/*
+*AUTORES:
+*Diego Rosales Peroza 
+*Laura Ruiz Mora 
+*Ángel Sanabria Suarez 
+*Martha Liliana Salazar Betancur 
+*Laura Vanessa Romero Jiménez 
+*/
+
 package tile;
 
 import java.awt.Graphics2D;
