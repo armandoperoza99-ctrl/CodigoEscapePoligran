36098df07e62b4843d8c1f08f6307d770f3b52f9
lvromjim <lauraromero0410@gmail.com>
2026-06-08 14:51:16 -0500

    Change tileM and player to public access modifiers
    Modificación requerida para la ejecución de las pruebas

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -24,10 +24,10 @@ public class GamePanel extends JPanel implements Runnable{
   //FPS
     int FPS = 60;
     
-    TileManager tileM = new TileManager(this);
+    public TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this,keyH);
+    public Player player = new Player(this,keyH);
     
     public GamePanel(){
 
